fc7dd29a0b0c7986440bc36c4cb778606087d2b1
Geovani Ladino <monkey.programmer7@gmail.com>
2022-10-28 03:45:15 -0500

    migraciones

---
+++ database/migrations/1666946562156_product_orders.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'product_orders'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('id_product').unsigned().references('id').inTable('products').onDelete('CASCADE')
+      table.integer('id_order').unsigned().references('id').inTable('orders').onDelete('CASCADE')
+      table.integer('quantity')
+      table.decimal('price').notNullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
