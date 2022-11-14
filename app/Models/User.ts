5693dfd27efde2287c879e37bce42742695e6f57
kevin.guerrero <kevinguerrero808@gmail.com>
2022-11-14 18:50:25 -0500

    hidden password

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -18,7 +18,7 @@ export default class User extends BaseModel {
   @column()
   public email:string;
 
-  @column()
+  @column({ serializeAs: null })
   public password:string;
 
   @column.dateTime({ autoCreate: true })
